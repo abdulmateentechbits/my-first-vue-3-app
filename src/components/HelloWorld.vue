bc4932e036479623742b46532affb1c8d422d0c9
abdulmateentechbits <mateen@techibits.com>
2023-08-30 14:41:37 +0500

    app changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,11 +18,6 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
